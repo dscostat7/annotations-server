e76ac0e264cbd0c7f07d3bf5c8c39b7d0b460ea1
Diego Souza <diegosouzagestor@gmail.com>
2024-09-10 11:29:09 -0300

    Created new routes Annotations

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,67 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../services/create-goal'
+import { getWeekPendingGoals } from '../services/get-week-pending-goals'
+import { createGoalCompletion } from '../services/create-goal-completion'
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.listen({
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalID: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalID } = request.body
+    const result = await createGoalCompletion({
+      goalID,
+    })
+
+    return result
+  }
+)
+
+app
+  .listen({
     port: 3333,
-}).then(() => {
-    console.log('HTTP server is runing');
-});
+  })
+  .then(() => {
+    console.log('HTTP server is runing')
+  })
---
+++ src/services/get-week-pending-goals.ts
@@ -0,0 +1,61 @@
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
+
+dayjs.extend(weekOfYear)
+
+export async function getWeekPendingGoals() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  )
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalID: goalCompletions.goalID,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalID)
+  )
+
+  const pendingGoals = await db
+    .with(goalsCreatedUpToWeek, goalCompletionCounts)
+    .select({
+      id: goalsCreatedUpToWeek.id,
+      title: goalsCreatedUpToWeek.title,
+      desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+      completionCount:
+        sql /*sql */`COALESCE(${goalCompletionCounts.completionCount}, 0)`.mapWith(
+          Number
+        ),
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(
+      goalCompletionCounts,
+      eq(goalCompletionCounts.goalID, goalsCreatedUpToWeek.id)
+    )
+
+  return {
+    pendingGoals,
+  }
+}
